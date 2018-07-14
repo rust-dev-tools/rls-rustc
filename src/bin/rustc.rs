8a7eb81623b5b2e2cc431b4d9eb2f8f64e6d375d
Eric Huss <eric@huss.org>
2018-07-13 20:31:56 -0700

    Fix stack overflow on windows.
    Fixes rust-lang-nursery/rls#838

--- src/bin/rustc.rs
+++ src/bin/rustc.rs
@@ -1,3 +1,10 @@
+// See rustc/rustc.rs in rust repo for explanation of stack adjustments.
+#![feature(link_args)]
+#[allow(unused_attributes)]
+#[cfg_attr(all(windows, target_env = "msvc"), link_args = "/STACK:16777216")]
+#[cfg_attr(all(windows, not(target_env = "msvc")), link_args = "-Wl,--stack,16777216")]
+extern {}
+
 extern crate rls_rustc;
 
 fn main() {
